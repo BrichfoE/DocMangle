bf45c6bcb1c388e3a8154cbb49580b91c743cf3e
TheBigEl <Elliott@ALPHA-DKNY322>
2018-06-16 09:18:46 -0400

    Readding try-catch in Program

--- DrMangle/Program.cs
+++ DrMangle/Program.cs
@@ -10,17 +10,17 @@
 
             while (activeGame)
             {
-                //try
-                //{
+                try
+                {
                     activeGame = gc.RunGame();
-                //}
-                //catch (System.Exception ex)
-                //{
-                //    string currentFile = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileName();
-                //    int currentLine = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileLineNumber();
-                //    gc.Repo.LogException(gc.Data, $"General exception {currentFile} line {currentLine}", ex, true);
-                //    activeGame = false;
-                //}
+                }
+                catch (System.Exception ex)
+                {
+                    string currentFile = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileName();
+                    int currentLine = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileLineNumber();
+                    gc.Repo.LogException(gc.Data, $"General exception {currentFile} line {currentLine}", ex, true);
+                    activeGame = false;
+                }
             }
         }
     }
